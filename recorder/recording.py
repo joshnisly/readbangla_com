1b263e8d7552f322af5fcdf841d20806a94b05f5
Josh Nisly <23241028+joshnisly@users.noreply.github.com>
2013-07-29 12:53:38 +0000

    Add recording client - initial import. Still quite rough.

---
+++ recorder/recording.py
@@ -0,0 +1,177 @@
+#!/usr/bin/python
+
+import array
+import threading
+import os
+import pyaudio
+import Queue
+import struct
+import sys
+import time
+import wave
+
+THRESHOLD = 5500
+CHUNK_SIZE = 1024
+FORMAT = pyaudio.paInt16
+RATE = 44100
+MAXIMUM = 16384
+
+class _RecordThread(threading.Thread):
+    def __init__(self, output_path, message_queue, parent):
+        threading.Thread.__init__(self)
+        self._output_path = output_path
+        self._message_queue = message_queue
+        self._parent = parent
+
+        self._pyaudio = pyaudio.PyAudio()
+        self._is_recording = False
+        self._stream = None
+        self._audio = array.array('h')
+        self._snd_started = False
+        self._num_silent = 0
+
+    def __del__(self):
+        self._pyaudio.terminate()
+
+    def run(self):
+        while True:
+            event = None
+            timeout = 0 if self._is_recording else 0.1
+            try:
+                event = self._message_queue.get(timeout=timeout)
+            except Queue.Empty:
+                pass
+            else:
+                if event is None:
+                    if self._is_recording:
+                        self._finish()
+                    break
+
+            if event == 'start':
+                self._start()
+
+            if event == 'stop':
+                self._finish()
+
+            if self._stream:
+                snd_data = array.array('h', self._stream.read(CHUNK_SIZE))
+                if sys.byteorder == 'big':
+                    snd_data.byteswap()
+
+                # See if we need to stop
+                silent = self._is_silent(snd_data)
+                if silent:
+                    if self._snd_started:
+                        self._num_silent += 1
+                else:
+                    if not self._snd_started:
+                        self._snd_started = True
+                    self._num_silent = 0
+
+                if self._snd_started and self._num_silent > 30:
+                    self._finish()
+                    continue
+
+                self._audio.extend(snd_data)
+
+    def is_recording(self):
+        return self._is_recording
+
+    def get_90th_percentile(self):
+        data = [abs(x) for x in list(self._audio)]
+        data.sort()
+        data = data[:len(data) / 10]
+        return data[-1]
+
+    def _start(self):
+        assert not self._is_recording
+        self._is_recording = True
+        self._audio = array.array('h')
+        self._stream = self._pyaudio.open(format=FORMAT, channels=1, rate=RATE,
+                                          input=True, output=True,
+                                          frames_per_buffer=CHUNK_SIZE)
+
+    def _finish(self):
+        assert self._is_recording
+
+        sample_width = self._pyaudio.get_sample_size(FORMAT)
+        self._stream.stop_stream()
+        self._stream.close()
+        self._stream = None
+
+        data = self._normalize(self._audio)
+        data = self._trim_silence(data)
+        data = struct.pack('<' + ('h'*len(data)), *data)
+
+        if os.path.exists(self._output_path):
+            os.unlink(self._output_path)
+        wf = wave.open(self._output_path, 'wb')
+        wf.setnchannels(1)
+        wf.setsampwidth(sample_width)
+        wf.setframerate(RATE)
+        wf.writeframes(data)
+        wf.close()
+
+        self._is_recording = False
+        self._snd_started = False
+        self._num_silent = 0
+        self._parent.on_recording_finish()
+
+    def _normalize(self, snd_data):
+        adjustment_factor = float(MAXIMUM)/max(abs(i) for i in snd_data)
+
+        normalized_data = array.array('h')
+        for i in snd_data:
+            normalized_data.append(int(i*adjustment_factor))
+        return normalized_data
+
+    def _trim_silence(self, snd_data):
+        def _trim(snd_data):
+            snd_started = False
+            r = array.array('h')
+
+            for i in snd_data:
+                if not snd_started and abs(i) > THRESHOLD:
+                    snd_started = True
+                    r.append(i)
+
+                elif snd_started:
+                    r.append(i)
+            return r
+
+        # Trim to the left
+        snd_data = _trim(snd_data)
+
+        # Trim to the right
+        snd_data.reverse()
+        snd_data = _trim(snd_data)
+        snd_data.reverse()
+        return snd_data
+
+    def _is_silent(self, snd_data):
+        return max(snd_data) < THRESHOLD
+
+class Recorder(object):
+    def __init__(self, output_path, parent):
+        self._message_queue = Queue.Queue()
+        self._record_thread = _RecordThread(output_path, self._message_queue, parent)
+        self._record_thread.start()
+
+    def start_stop(self):
+        if self.is_recording():
+            self._message_queue.put('stop')
+        else:
+            self._message_queue.put('start')
+
+        time.sleep(0.1)
+
+    def get_90th_percentile(self):
+        return self._record_thread.get_90th_percentile()
+
+    def is_recording(self):
+        return self._record_thread.is_recording()
+
+    def quit(self):
+        self._message_queue.put(None)
+
+
---
+++ recorder/uploading.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+import httplib
+import threading
+import Queue
+
+HOST = 'www.readbangla.com'
+PORT = 80
+PATH = '/upload_recording/'
+
+class _UploadThread(threading.Thread):
+    def __init__(self, message_queue, parent):
+        threading.Thread.__init__(self)
+        self._message_queue = message_queue
+        self._parent = parent
+
+    def run(self):
+        try:
+            while True:
+                file_path = self._message_queue.get()
+                if file_path is None:
+                    break
+
+                self._upload_file(file_path)
+                time.sleep(0.1)
+        except Exception, e:
+            text = str(e)
+            self._parent.on_error(text)
+
+
+    def _upload_file(self, file_path):
+        try:
+            conn = httplib.HTTPConnection(HOST, PORT)
+            body = open(file_path, 'rb')
+            conn.request('POST', PATH, body=body)
+            res = conn.getresponse()
+        except Exception, e:
+            print e
+            self._message_queue.put(file_path)
+            return
+
+        os.unlink(file_path)
+        
+
+class Uploader(object):
+    def __init__(self, parent):
+        self._message_queue = Queue.Queue()
+        self._upload_thread = _UploadThread(self._message_queue, parent)
+        self._upload_thread.start()
+
+    def add_item(self, file_path):
+        self._message_queue.put(file_path)
+
+    def quit(self):
+        self._message_queue.put(None)
+
