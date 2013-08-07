48bb9680d980d787998c7b009b390ac2951f0c40
Josh Nisly <23241028+joshnisly@users.noreply.github.com>
2013-08-07 13:23:57 +0000

    Make SVG icons work on Py2exe images.

--- recorder/setup.py
+++ recorder/setup.py
@@ -5,10 +5,21 @@ import os
 import PyQt4
 import shutil
 import sys
+import zipfile
 
 from distutils.core import setup
 from distutils.cmd import Command
 import py2exe #pylint: disable=F0401
+
+def _zip_dir(path, rel_zip_path, output_path):
+    assert os.path.isdir(path)
+    with zipfile.ZipFile(output_path, "w", zipfile.ZIP_DEFLATED) as zip_file:
+        for root, dirs, files in os.walk(path):
+            for entry in files:
+                full_path = os.path.join(root, entry)
+                assert full_path.startswith(path + os.sep)
+                rel_path = rel_zip_path + full_path[len(path):]
+                zip_file.write(full_path, rel_path)
 
 if __name__ == '__main__':
     pyqt_dir = os.path.dirname(PyQt4.__file__)
@@ -20,7 +31,7 @@ if __name__ == '__main__':
                      {'excludes': ['pywin', 'pywin.debugger', 'pywin.debugger.dbgcon',
                                   'pywin.dialogs', 'pywin.dialogs.list',
                                   'Tkconstants','Tkinter','tcl'],
-                      'includes': ['sip', 'pyaudio'],
+                      'includes': ['sip', 'pyaudio', 'PyQt4.QtSvg', 'PyQt4.QtXml'],
                       'dll_excludes': ['w9xpopen.exe', 'mswsock.dll'],
                       }},
           data_files = [
@@ -33,5 +44,7 @@ if __name__ == '__main__':
                      }],
           zipfile = None
           )
+
+    _zip_dir('dist', 'BanglaRecorder', 'BanglaRecorder.zip')
 
 
